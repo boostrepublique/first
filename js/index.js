615b5c77833847af0c7d9cde92ec5ad1094e6e85
boostrepublique <guiome.flandre+boostrepublique@gmail.com>
2015-09-05 21:54:57 +0200

    JS fixes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 jQuery('document').ready(function () {
-  setSizes();
-
   var images = jQuery('.image-loader');
+
   for (var i = 0; i < images.length; i++) {
-    imageLoader({
-      element: images.eq(i),
-      onLoad: setSizes
-    }).loadBackgroundPhoto();
+    (function(image) {
+      imageLoader({
+        element: image,
+        onLoad: function () {
+          setSize(image);
+        }
+      }).loadBackgroundPhoto();
+    })(images.eq(i));
   }
 
   jQuery(window).on('resize', setSizes);
@@ -23,8 +26,24 @@ jQuery(window).load(function () {
   }
 });
 
-var setSizes = function () {
-  var height = jQuery('.image-loader').eq(0).width() / 1.5;
+var setSize = function (image) {
+  var height = (jQuery('.album').width() - 20) / 1.5;
+
+  if (height > window.innerHeight) {
+    image.height(window.innerHeight - 20)
+         .find('.image').height(window.innerHeight - 20);
+    image.width((window.innerHeight - 20) * 1.5)
+         .find('.image').width((window.innerHeight - 20) * 1.5);
+  } else {
+    image.css('width', '100%')
+         .find('.image').css('width', '100%');
+    image.height(height)
+         .find('.image').height(height);
+  }
+};
+
+var setSizes = function (image) {
+  var height = (jQuery('.album').width() - 20) / 1.5;
 
   if (height > window.innerHeight) {
     jQuery('.image-loader, .image').height(window.innerHeight - 20);
