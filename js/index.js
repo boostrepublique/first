19b434c1c1fc3800ff6f4894d3b438063c1d2742
boostrepublique <guiome.flandre+boostrepublique@gmail.com>
2015-09-06 21:10:09 +0200

    fixes

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ jQuery('document').ready(function () {
     })(images.eq(i));
   }
 
-//  jQuery(window).on('resize', setSizes);
+  jQuery(window).on('resize', setSizes);
   jQuery(window).on('orientationchange', setSizes);
 });
 
@@ -29,28 +29,11 @@ jQuery(window).load(function () {
 
 var setSize = function (image) {
   var height = (jQuery('.album').width()) / 1.5;
-
-  if (height > window.innerHeight) {
-    image.height(window.innerHeight - 20)
-         .find('.image').height(window.innerHeight - 20);
-    image.width((window.innerHeight - 20) * 1.5)
-         .find('.image').width((window.innerHeight - 20) * 1.5);
-  } else {
-    image.css('width', '100%')
-         .find('.image').css('width', '100%');
-    image.height(height)
-         .find('.image').height(height);
-  }
+  image.height(height)
+       .find('.image').height(height);
 };
 
 var setSizes = function (image) {
   var height = (jQuery('.album').width() - 20) / 1.5;
-
-  if (height > window.innerHeight) {
-    jQuery('.image-loader, .image').height(window.innerHeight - 20);
-    jQuery('.image-loader, .image').width((window.innerHeight - 20) * 1.5);
-  } else {
-    jQuery('.image-loader, .image').width('100%');
-    jQuery('.image-loader, .image').height(jQuery('.image-loader').eq(0).width() / 1.5);
-  }
+  jQuery('.image-loader, .image').height(jQuery('.image-loader').eq(0).width() / 1.5);
 };
